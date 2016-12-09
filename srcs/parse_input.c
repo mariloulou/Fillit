7632a196face540d4bc6da16d1428469063f1158
gudemare <gudemare@student.42.fr>
2016-12-09 17:56:29 +0100

    Tetriminos are pulled up left on parsing

--- srcs/parse_input.c
+++ srcs/parse_input.c
@@ -6,7 +6,7 @@
 /*   By: gudemare <gudemare@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/07 11:28:22 by gudemare          #+#    #+#             */
-/*   Updated: 2016/12/08 15:30:13 by gudemare         ###   ########.fr       */
+/*   Updated: 2016/12/09 17:51:51 by gudemare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,8 +27,35 @@
 ** parse_tetriminos() runs at the start of a complete, correctly formated
 ** tetrimino.
 ** parse_tetriminos_block() is a recursive function to check the tetrimino
-** itself. It transforms the '#' in digits as it goes.
+** itself. It transforms the '#' in letters as it goes.
+** tidy_tetriminos() pulls the tetriminos in the upper left corner.
 */
+
+static void	tidy_tetriminos(char *s)
+{
+	int		i;
+
+	while (s[0] == '.' && s[1] == '.' && s[2] == '.' && s[3] == '.')
+	{
+		i = 0;
+		while (i < 20)
+		{
+			if ((i + 1) % 5 != 0)
+				s[i] = (i < 15) ? s[i + 5] : '.';
+			i++;
+		}
+	}
+	while (s[0] == '.' && s[5] == '.' && s[10] == '.' && s[15] == '.')
+	{
+		i = 0;
+		while (i < 20)
+		{
+			if ((i + 1) % 5 != 0)
+				s[i] = ((i + 2) % 5 != 0) ? s[i + 1] : '.';
+			i++;
+		}
+	}
+}
 
 static int	parse_tetriminos_block(char *s, int i, int tetri_nb)
 {
@@ -60,6 +87,7 @@ static int	parse_tetriminos(char *s)
 		i++;
 	if (parse_tetriminos_block(s, i, tetri_nb) != 4)
 		return (0);
+	tidy_tetriminos(s);
 	return (1);
 }
 
@@ -86,7 +114,7 @@ static int	verify_format(char *s)
 		}
 		if (!parse_tetriminos(s) || (s[i] != '\n' && s[i] != '\0'))
 			return (0);
-		s += i + 1;
+		s += 21;
 	}
 	return (1);
 }
