181cce8aa364492b72ce3a0a9b3f8b61cb123781
Marilou CASSAR <mcassar@e3r8p20.42.fr>
2016-12-10 18:29:12 +0100

    place_tetriminos (with main test)

---
+++ srcs/place_tetriminos.c
@@ -0,0 +1,99 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   place_tetriminos.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcassar <mcassar@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/12/10 14:23:05 by mcassar           #+#    #+#             */
+/*   Updated: 2016/12/10 18:27:57 by mcassar          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+#include "libft.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int	place_tetri(char **map, int i, int j, char *tetri)
+{
+	if (map[i][j] != '.' || *tetri == '.')
+		return (1);
+	if (map[i][j] == '.' && *tetri != '.')
+		map[i][j] = *tetri;
+	return (0);
+}
+
+void	check_place_ext(t_struct *s, int *j)
+{
+	*j = *j + 1;
+	s->block = s->block + 1;
+}
+
+int		check_place(char **map, int i, int j, char *tetri)
+{
+	t_struct s;
+	
+	s.savej = j;
+	s.block = 0;
+	while (s.block < 4 && *tetri != '\0')
+	{
+		while (*tetri == '.')
+		{
+			tetri++;
+			(map[i][j] == '.') ? j++ : 0;
+		}
+		if (place_tetri(map, i, j, tetri) == 1)
+			return(0);
+		check_place_ext(&s, &j);
+		tetri++;
+		while (*tetri == '.')
+			tetri++;
+		if (*tetri == '\n')
+		{
+			i++;
+			j = s.savej;
+			tetri++;
+		}
+	}
+	return (s.block);
+}
+
+int		valid_place(char **map, int i, int j, char *tetri)
+{
+	if (check_place(map, i, j, tetri) == 4)
+		return(0);
+	else
+		return(1);
+}
+
+int		main(void)
+{
+	char	**map;
+	char	*tetri;
+	int		i;
+	int		j;
+	int		fd;
+	char	buf[4096 + 1];
+	int		h;
+
+	h = 0;
+	i = 0;
+	j = 0;
+	fd = open("map", O_RDWR);
+	read(fd, buf, 4096);
+	map = ft_strsplit(buf, '\n');
+	tetri = ".#.\n###.\n....\n....";
+	if (valid_place(map, i, j, tetri) == 0)
+	{
+		while (map[h])
+		{
+			ft_putendl(map[h]);
+			h++;
+		}
+		return (0);
+	}
+	return (1);
+}
